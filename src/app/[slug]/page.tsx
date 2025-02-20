8ad62a9528d19315c5da982d5c010902b46d8ed2
Laura Perroni <lauraperroni2020@gmail.com>
2025-02-20 09:23:01 -0300

    feat: migration and slug page

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,11 @@
+interface RestaurantPageProps {
+  params: Promise<{ slug: string }>;
+}
+
+const RestaurantPage = async ({ params }: RestaurantPageProps) => {
+  const { slug } = await params;
+
+  return <h1>{slug}</h1>;
+};
+
+export default RestaurantPage;
