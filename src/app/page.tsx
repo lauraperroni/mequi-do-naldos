77987336e6d407dfea8597d73b56b4b20024ced0
Laura Perroni <lauraperroni2020@gmail.com>
2025-02-25 13:26:09 -0300

    fix: merge conflicts 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
+import { useParams } from "next/navigation";
+import router from "next/router";
+
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
-  return <h1>hello world</h1>;
+  const { slug } = useParams<{ slug: string }>();
+  const handleOrdersClick = () => router.push(`/${slug}`);
+
+  return (
+    <>
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute right-4 top-4 z-50 rounded-full"
+        onClick={handleOrdersClick}
+      ></Button>
+    </>
+  );
 };
 
 export default HomePage;
